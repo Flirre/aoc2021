d8bf99efc89869148f11afb98460a71f4f679931
Filip Lindahl <filip.g.lindahl@gmail.com>
2021-12-01 18:16:47 +0100

    lose some lines

--- day01/index.js
+++ day01/index.js
@@ -17,8 +17,7 @@ function getSolutionPart2() {
   let increase = 0;
   for (let i = 0; i < input.length - 3; i++) {
     if (
-      input[i] + input[i + 1] + input[i + 2] <
-      input[i + 1] + input[i + 2] + input[i + 3]
+      (input[i] + input[i + 1] + input[i + 2]) < (input[i + 1] + input[i + 2] + input[i + 3])
     )
       increase++;
   }
@@ -28,9 +27,3 @@ function getSolutionPart2() {
 const part = process.env.part || "part1";
 if (part === "part1") console.log(getSolutionPart1());
 else console.log(getSolutionPart2());
-
-module.exports = {
-  getSolutionPart1,
-  getSolutionPart2,
-  inputDataLinesIntegers,
-};
